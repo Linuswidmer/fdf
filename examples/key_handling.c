fd7ba365340bb828d2127f7ceb76e38b52528a31
linuswidmer <lwidmer.hb@gmail.com>
2023-01-24 15:10:27 +0100

    cross

--- examples/key_handling.c
+++ examples/key_handling.c
@@ -1,11 +1,35 @@
 #include <mlx.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
 typedef struct s_vars{
   void *mlx;
   void *win;
 } t_vars;
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct s_point {
+  float x;
+  float y;
+}       t_point;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+  // printf("DST = %s\n", dst);
+	*(unsigned int*)dst = color;
+  // printf("DST = %s\n", dst);
+}
 
 int close(int keycode, t_vars *vars)
 {
@@ -16,23 +40,38 @@ int close(int keycode, t_vars *vars)
   if (keycode == 65307)
   {
     // printf("%p\n", vars->mlx);
-    mlx_destroy_window(vars->mlx, vars->win);
     mlx_loop_end(vars->mlx);
     // printf("%p\n", vars->mlx);
     // ptr = NULL;
-    vars->win = NULL;
   }
+  return (0);
+}
+
+int cross(t_vars *vars)
+{
+    mlx_loop_end(vars->mlx);
+
   return (0);
 }
 
 int	main(void)
 {
   t_vars vars;
+  t_data img;
+  // int ptr;
 
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, 400, 400, "Hello world!");
-    printf("%p\n", vars.mlx);
+	img.img = mlx_new_image(vars.mlx, 120, 120);
+  img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+  my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+  mlx_destroy_image(vars.mlx, &img);
+  mlx_put_image_to_window(vars.mlx, vars.win, img.img, 10, 10);
   mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+  mlx_hook(vars.win, 17, 1L<<2, cross, &vars);
 	mlx_loop(vars.mlx);
+  // mlx_destroy_image(vars.mlx, &img);
+  mlx_destroy_window(vars.mlx, vars.win);
   printf("exited the loop\n");
+  // ptr = malloc(2);
 }
